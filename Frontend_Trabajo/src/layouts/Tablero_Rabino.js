7219368d2e78432c248799aed0f395f0af541ef1
luciaarribas <827183@unizar.es>
2023-04-20 15:40:08 +0200

    añadidas imagenes de las cartas

--- Frontend_Trabajo/src/layouts/Tablero_Rabino.js
+++ Frontend_Trabajo/src/layouts/Tablero_Rabino.js
@@ -3,7 +3,7 @@ import "./../assets/css/cartas_rabino.css";
 import CardsWrapper from "components/Cartas_Rabino/CardWrapper.js";
 
 function App() {
-  const cartas_inicio = [
+  const cartas_mano = [
     {
       number: 0, //JOKER
       symbol: 0,
@@ -21,9 +21,75 @@ function App() {
       symbol: 4,
     },
   ];
+
+  const CSelecion= [];
+
+  const t = [
+    [{
+      number: 0, //JOKER
+      symbol: 0,
+    },
+    {
+      number: 11, //JOKER
+      symbol: 1,
+    },
+    {
+      number: 11, //JOKER
+      symbol: 2,
+    }
+    ],
+    [{
+      number: 11, //Q
+      symbol: 3,
+    },
+    {
+      number: 11, //Q
+      symbol: 4,
+    },
+    {
+      number: 12, //Q
+      symbol: 1,
+    }
+    ],
+    [{
+      number: 12, //J
+      symbol: 2,
+    },
+    {
+      number: 12, //J
+      symbol: 3,
+    },
+    {
+      number: 12, //J
+      symbol: 4,
+    },
+    ],
+    [{
+      number: 13, //K
+      symbol: 1,
+    },
+    {
+      number: 13, //K
+      symbol: 2,
+    },
+    {
+      number: 13, //K
+      symbol: 3,
+    },
+    {
+      number: 13, //K
+      symbol: 4,
+    }
+    ]
+  ];
   return (
     <div className="App">
-      <CardsWrapper cartas = {cartas_inicio} cardsNumber = {cartas_inicio.length} />
+      {t.map((fila, i) => (
+        <div key={i} style={{ display: 'flex', flexDirection: 'row' }}>
+          <CardsWrapper cartas={fila} cardsNumber={fila.length} />
+        </div>
+      ))}
+      <CardsWrapper cartas = {cartas_mano} cardsNumber = {cartas_mano.length} />
       <button onClick={() => window.location.reload()}>Reload Cards</button>
     </div>
   );
