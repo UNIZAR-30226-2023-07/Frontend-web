9df4b178fd6c828d150925ca7436f31b87b1a083
luciaarribas <827183@unizar.es>
2023-04-21 00:03:53 +0200

    Muestra las cartas en el tablero

--- Frontend_Trabajo/src/layouts/Tablero_Rabino.js
+++ Frontend_Trabajo/src/layouts/Tablero_Rabino.js
@@ -1,8 +1,14 @@
 import "./../assets/css/cartas_rabino.css";
 //import Card from "components/Cartas_Rabino/card.js";
+import React, { useEffect } from 'react';
 import CardsWrapper from "components/Cartas_Rabino/CardWrapper.js";
+import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
 
 function App() {
+  useEffect(() => {
+    // Set the overflow property to hidden on the body element
+    document.body.style.overflow = 'hidden';
+  }, []);
   const cartas_mano = [
     {
       number: 0, //JOKER
@@ -84,15 +90,34 @@ function App() {
   ];
   return (
     <div className="App">
-      {t.map((fila, i) => (
-        <div key={i} style={{ display: 'flex', flexDirection: 'row' }}>
-          <CardsWrapper cartas={fila} cardsNumber={fila.length} />
+      <div style={{ display: 'flex', width: '100%', flexDirection: 'row' }}>
+        <div style={{ flexGrow: 1 }}>
+          <div style={{backgroundColor: 'green', height: 'calc(95vh - 15%)',width: '100%', overflowY: 'scroll' }}>
+            {t.map((fila, i) => (
+              <div key={i} style={{ display: 'flex', flexDirection: 'row',borderBottom: '1px solid white' }}>
+                <CardsWrapper cartas={fila} cardsNumber={fila.length} />
+              </div>
+            ))}
+          </div>
+          <div style={{ backgroundColor: 'brown', padding: '10px', width: '100%' }}>
+            <CardsWrapper cartas={cartas_mano} cardsNumber={cartas_mano.length} />
+          </div>
+          <button onClick={() => window.location.reload()}>Reload Cards</button>
         </div>
-      ))}
-      <CardsWrapper cartas = {cartas_mano} cardsNumber = {cartas_mano.length} />
-      <button onClick={() => window.location.reload()}>Reload Cards</button>
+        <div style={{ backgroundColor: 'grey', width: '20%', minWidth: '200px' }}>
+          <Row>
+          <button onClick={() => console.log('Nueva combinacion')}>Nueva combinación</button>
+          </Row>
+          <Row>
+          <button onClick={() => console.log('Abrir chat')}>Abrir chat</button>
+          </Row>
+          
+        </div>
+      </div>
     </div>
   );
+  
+  
 }
 
 export default App;
