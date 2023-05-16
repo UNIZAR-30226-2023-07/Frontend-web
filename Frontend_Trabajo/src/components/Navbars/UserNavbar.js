ea276833818b43ea7239d40a8c636a58c77ca6e6
815743 <815743@unizar.es>
2023-05-16 17:20:13 +0200

    Update UserNavbar.js

--- Frontend_Trabajo/src/components/Navbars/UserNavbar.js
+++ Frontend_Trabajo/src/components/Navbars/UserNavbar.js
@@ -194,29 +194,6 @@ const UserNavbar = (props) => {
                   <i className="ni ni-user-run" />
                   <span>Cerrar sesión</span>
                 </DropdownItem>
-                <DropdownItem divider /> {/*Esto solo es para borrar sessionStorage de la Partida de forma rápida */}
-                <DropdownItem onClick={() =>{   sessionStorage.removeItem('juego7reinas');
-                                                sessionStorage.removeItem('pConectada7reinas');
-                                                sessionStorage.removeItem('jugadorxs7reinas');
-                                                sessionStorage.removeItem('jGetUser7reinas');
-                                                sessionStorage.removeItem('miturno7reinas');
-                                                sessionStorage.removeItem('mano7reinas');
-                                                sessionStorage.removeItem('tablero7reinas');
-                                                sessionStorage.removeItem('descarte7reinas');
-                                                sessionStorage.removeItem('turno7reinas');
-                                                sessionStorage.removeItem('heabierto7reinas');
-                                                sessionStorage.removeItem('herobado7reinas');
-                                                sessionStorage.removeItem('part_pausadas7reinas');
-                                                sessionStorage.removeItem("es_torneo7reinas");
-                                                sessionStorage.removeItem("puntosTorneo7reinas");
-                                                sessionStorage.removeItem("ganadorxronda7reinas");
-                                                sessionStorage.removeItem("ganadorx7reinas");
-                                                sessionStorage.removeItem("torneo_ganado7reinas");
-                                                sessionStorage.removeItem("reanudada7reinas");
-                                                console.log("SE HAN BORRADO LAS VARIABLES DE PARTIDA")}}>
-                  <i className="ni ni-user-run" />
-                  <span>Borrar S Partida</span>
-                </DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
           </Nav>
